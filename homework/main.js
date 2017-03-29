d307f4cea73d4e0bda812d50251637c3677502ab
Yaroslava <yara.icecat@gmail.com>
2017-03-29 10:12:21 +0300

    homework

--- homework/main.js
+++ homework/main.js
@@ -3,17 +3,47 @@
 window.onload = function() {
     
     function DomManipulation() {
+
+        // метод записи отредактированных данных из textarea
         document.querySelector('#okBtn').addEventListener('click', function(){
             var textarea = document.querySelector('#textarea');
+
+            data = DomManipulation.prototype.checkData(data);
+
             data[textarea.dataset.obj][textarea.dataset.key] = textarea.value;
             DomManipulation.prototype.checkState();
             textarea.value = '';
         });
+
+        // метод добавления новых строк
+        document.querySelector('#addRow').addEventListener('click', function(){
+
+            data = DomManipulation.prototype.checkData(data);
+
+            var tableItem = data[0];
+            var obj = {};
+
+            for(var item in tableItem) {
+                obj[item] = '';
+            }
+            data.push(obj);
+
+            DomManipulation.prototype.checkState();
+        });
+
+        // метод очистки localStorage
+        document.querySelector('#clearStorage').addEventListener('click', function(){
+            localStorage.clear();
+            DomManipulation.prototype.checkData();
+            DomManipulation.prototype.checkState();
+        });
     }
     
     // метод перерисовки таблицы
-    DomManipulation.prototype.renderTable = function(toElement, tableList, nameTable) {
-        
+    DomManipulation.prototype.renderTable = function(toElement, data, nameTable) {
+        // проверяем есть ли запись в localStorage
+        data = DomManipulation.prototype.checkData(data);
+
         if(document.querySelector('#' + nameTable)) {
             document.querySelector('#' + nameTable).remove();
         }
@@ -24,9 +54,9 @@ window.onload = function() {
         toElement.appendChild(table);
         
         // в таблицу вставляем ряд с заголовками
-        this.renderTableHeader(table, tableList[0]);
+        this.renderTableHeader(table, data[0]);
         // в таблицу вставляем ряды с ячейками
-        this.renderTableCells(table, tableList);
+        this.renderTableCells(table, data);
         
     }
     
@@ -40,6 +70,11 @@ window.onload = function() {
             th.innerHTML = item;
             tr.appendChild(th);
         }
+
+        // добавляем доп. столбец с кнопками для удаления рядов
+        var th = document.createElement('th');
+        tr.appendChild(th);
+
         toTable.appendChild(tr);
     }
     
@@ -68,15 +103,44 @@ window.onload = function() {
                     textarea.dataset.obj = this.parentNode.dataset.position;
                 });
             }
-            
+            // создаём ячейки с кнопками для удаления рядов
+            var td = document.createElement('td');
+            var removeBtn = document.createElement('button');
+            removeBtn.classList.add('removeRow');
+            removeBtn.innerHTML = 'X';
+            td.appendChild(removeBtn);
+            tr.appendChild(td);
+
+            // событие удаления строк и перезаписи data
+            removeBtn.addEventListener('click',function(){
+                var index = this.parentNode.parentNode.dataset.position;
+
+                data = DomManipulation.prototype.checkData(data);
+
+                data.splice(index,1);
+                table.removeChild(this.parentNode.parentNode);
+                DomManipulation.prototype.checkState();
+            });
+
             toTable.appendChild(tr);
         }
     }
     
     // метод обновления данных
     DomManipulation.prototype.checkState = function() {
-        this.renderTable(container, data, 'table');
+        // добавляем запись в localStorage
         IconJson = JSON.stringify(data);
+        localStorage.setItem('data',IconJson);
+
+        this.renderTable(container, data, 'table');
+    }
+
+    // метод проверки наличия записи в localStorage
+    DomManipulation.prototype.checkData = function(data) {
+        if(localStorage['data']) {
+            data = JSON.parse(localStorage['data']);
+        }
+        return data;
     }
     
     // метод сортировки по числовому полю
@@ -116,8 +180,4 @@ window.onload = function() {
         /*domExemplair.sortByField(table);*/
     },2000);
     
-    // дз: справа от таблицы столбец с кнопками удалить ряд
-    // добавить кнопку добавления пустого ряда с записью в JSON
-    // запись в локалсторедж
-    // добавить кнопку очистки стореджа
 }
